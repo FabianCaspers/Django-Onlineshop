84c77b23caeeba94ba264439ceb78a391a5dabe0
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-20 13:51:32 +0200

    badge anzahl dynamisch

---
+++ shop/context_processors.py
@@ -0,0 +1,16 @@
+from . models import Bestellung, BestellteArtikel
+
+def warenkorb_anzahl(request):
+    if request.user.is_authenticated:
+        kunde = request.user.kunde
+        bestellung = Bestellung.objects.get(kunde=kunde, erledigt=False)
+        
+        if bestellung:
+            menge = bestellung.get_gesamtmenge
+        else:
+            megen = 0
+        
+    else:
+        menge = 0
+        
+    return {'menge': menge}
