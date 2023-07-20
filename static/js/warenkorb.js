977a6f4c9678897a72af240dee8fca00881c59a1
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-20 12:06:56 +0200

    add javascript

---
+++ static/js/warenkorb.js
@@ -0,0 +1,10 @@
+let bestellenButtons = document.getElementsByClassName("warenkorb-bestellen");
+
+for (let i = 0; i < bestellenButtons.length; i++) {
+    bestellenButtons[i].addEventListener('click', function(){
+        let artikelID = this.dataset.artikel;
+        let action = this.dataset.action;
+        
+    })
+    
+}
